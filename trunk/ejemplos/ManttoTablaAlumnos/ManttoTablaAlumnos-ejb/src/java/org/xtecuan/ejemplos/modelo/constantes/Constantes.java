106818a939425fb1bdc6cd0b90935a06da4a94a4
xtecuan@gmail.com <xtecuan@gmail.com@fb5652c3-6964-3999-2282-6a43b31b4ca0>
2012-10-22 22:08:44 +0000

    Cambios Delete

---
+++ trunk/ejemplos/ManttoTablaAlumnos/ManttoTablaAlumnos-ejb/src/java/org/xtecuan/ejemplos/modelo/constantes/Constantes.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.xtecuan.ejemplos.modelo.constantes;
+
+/**
+ *
+ * @author xtecuan
+ */
+public final class Constantes {
+
+    public static final int FALLO_AL_BORRAR = -1000;
+    public static final int EXITO_AL_BORRAR = 0;
+}
