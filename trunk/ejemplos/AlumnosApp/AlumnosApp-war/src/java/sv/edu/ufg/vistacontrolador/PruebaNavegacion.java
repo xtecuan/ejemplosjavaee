fd4273700ab077fe87be7b512f2430d8455321df
xtecuan@gmail.com <xtecuan@gmail.com@fb5652c3-6964-3999-2282-6a43b31b4ca0>
2012-10-27 20:28:49 +0000

    trunk/ejemplos/AlumnosApp

---
+++ trunk/ejemplos/AlumnosApp/AlumnosApp-war/src/java/sv/edu/ufg/vistacontrolador/PruebaNavegacion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ufg.vistacontrolador;
+
+/**
+ *
+ * @author ufg
+ */
+public class PruebaNavegacion {
+
+    /**
+     * Creates a new instance of PruebaNavegacion
+     */
+    public PruebaNavegacion() {
+    }
+
+    public String irPagina2() {
+        
+        /*
+         * Codigo java de inicializacion
+         */
+        
+        return "pagina2?faces-redirect=true";
+    }
+}
