1fcd56bd449f6da25c7087e460a0bf0a023a5896
xtecuan@gmail.com <xtecuan@gmail.com@fb5652c3-6964-3999-2282-6a43b31b4ca0>
2013-03-05 01:17:23 +0000

    trunk/ejemplos/maestria_ufg/HolaMundo importando al repositorio de codigo de subversion

---
+++ ejemplos/maestria_ufg/HolaMundo/src/sv/edu/ufg/ejemplos/holamundo/UsarHolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sv.edu.ufg.ejemplos.holamundo;
+
+/**
+ *
+ * @author javaee
+ */
+public class UsarHolaMundo {
+
+    
+    
+    public static void main(String[] args) {
+        HolaMundoJava mundo = new HolaMundoJava();
+
+        mundo.main(null);
+    }
+}
