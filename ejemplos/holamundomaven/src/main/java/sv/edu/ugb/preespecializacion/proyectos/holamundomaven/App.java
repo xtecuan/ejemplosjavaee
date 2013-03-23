65ae2710f07302cedef101b49defe58d942726f5
xtecuan <xtecuan@fb5652c3-6964-3999-2282-6a43b31b4ca0>
2013-03-23 16:44:29 +0000

    trunk/ejemplos/holamundomaven

---
+++ ejemplos/holamundomaven/src/main/java/sv/edu/ugb/preespecializacion/proyectos/holamundomaven/App.java
@@ -0,0 +1,18 @@
+package sv.edu.ugb.preespecializacion.proyectos.holamundomaven;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    
+    private static Logger logger = Logger.getLogger(App.class);
+    
+    public static void main( String[] args )
+    {
+        logger.info( "Hello World!" );
+    }
+}
