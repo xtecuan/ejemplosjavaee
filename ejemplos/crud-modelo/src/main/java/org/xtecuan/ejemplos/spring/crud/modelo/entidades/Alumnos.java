305f78d7328f60041f97ad84165df12c39e9c3ec
xtecuan@gmail.com <xtecuan@gmail.com@fb5652c3-6964-3999-2282-6a43b31b4ca0>
2013-04-28 07:29:23 +0000

    trunk/ejemplos/crud-modelo

---
+++ ejemplos/crud-modelo/src/main/java/org/xtecuan/ejemplos/spring/crud/modelo/entidades/Alumnos.java
@@ -0,0 +1,146 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.xtecuan.ejemplos.spring.crud.modelo.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author xtecuan
+ */
+@Entity
+@Table(name = "alumnos", catalog = "alumnos", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumnos.findAll", query = "SELECT a FROM Alumnos a"),
+    @NamedQuery(name = "Alumnos.findById", query = "SELECT a FROM Alumnos a WHERE a.id = :id"),
+    @NamedQuery(name = "Alumnos.findByCarnet", query = "SELECT a FROM Alumnos a WHERE a.carnet = :carnet"),
+    @NamedQuery(name = "Alumnos.findByNombres", query = "SELECT a FROM Alumnos a WHERE a.nombres = :nombres"),
+    @NamedQuery(name = "Alumnos.findByApellidos", query = "SELECT a FROM Alumnos a WHERE a.apellidos = :apellidos"),
+    @NamedQuery(name = "Alumnos.findByCorreo", query = "SELECT a FROM Alumnos a WHERE a.correo = :correo"),
+    @NamedQuery(name = "Alumnos.findByFechanac", query = "SELECT a FROM Alumnos a WHERE a.fechanac = :fechanac")})
+public class Alumnos implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "carnet", nullable = false, length = 10)
+    private String carnet;
+    @Basic(optional = false)
+    @Column(name = "nombres", nullable = false, length = 50)
+    private String nombres;
+    @Basic(optional = false)
+    @Column(name = "apellidos", nullable = false, length = 50)
+    private String apellidos;
+    @Column(name = "correo", length = 50)
+    private String correo;
+    @Column(name = "fechanac")
+    @Temporal(TemporalType.DATE)
+    private Date fechanac;
+
+    public Alumnos() {
+    }
+
+    public Alumnos(Integer id) {
+        this.id = id;
+    }
+
+    public Alumnos(Integer id, String carnet, String nombres, String apellidos) {
+        this.id = id;
+        this.carnet = carnet;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Date getFechanac() {
+        return fechanac;
+    }
+
+    public void setFechanac(Date fechanac) {
+        this.fechanac = fechanac;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumnos)) {
+            return false;
+        }
+        Alumnos other = (Alumnos) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.xtecuan.ejemplos.spring.crud.modelo.entidades.Alumnos[ id=" + id + " ]";
+    }
+    
+}
---
+++ ejemplos/crud-modelo/src/main/java/org/xtecuan/ejemplos/spring/crud/modelo/entidades/Libros.java
@@ -0,0 +1,147 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.xtecuan.ejemplos.spring.crud.modelo.entidades;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author xtecuan
+ */
+@Entity
+@Table(name = "Libros", catalog = "alumnos", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Libros.findAll", query = "SELECT l FROM Libros l"),
+    @NamedQuery(name = "Libros.findById", query = "SELECT l FROM Libros l WHERE l.id = :id"),
+    @NamedQuery(name = "Libros.findByCodigo", query = "SELECT l FROM Libros l WHERE l.codigo = :codigo"),
+    @NamedQuery(name = "Libros.findByNombre", query = "SELECT l FROM Libros l WHERE l.nombre = :nombre"),
+    @NamedQuery(name = "Libros.findByAutor", query = "SELECT l FROM Libros l WHERE l.autor = :autor"),
+    @NamedQuery(name = "Libros.findByIsbn", query = "SELECT l FROM Libros l WHERE l.isbn = :isbn"),
+    @NamedQuery(name = "Libros.findByFechaImpresion", query = "SELECT l FROM Libros l WHERE l.fechaImpresion = :fechaImpresion"),
+    @NamedQuery(name = "Libros.findByEstado", query = "SELECT l FROM Libros l WHERE l.estado = :estado")})
+public class Libros implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+    @Column(name = "codigo", length = 10)
+    private String codigo;
+    @Column(name = "nombre", length = 100)
+    private String nombre;
+    @Column(name = "autor", length = 75)
+    private String autor;
+    @Column(name = "ISBN", length = 10)
+    private String isbn;
+    @Column(name = "FechaImpresion")
+    @Temporal(TemporalType.DATE)
+    private Date fechaImpresion;
+    @Column(name = "estado")
+    private Character estado;
+
+    public Libros() {
+    }
+
+    public Libros(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getFechaImpresion() {
+        return fechaImpresion;
+    }
+
+    public void setFechaImpresion(Date fechaImpresion) {
+        this.fechaImpresion = fechaImpresion;
+    }
+
+    public Character getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Character estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Libros)) {
+            return false;
+        }
+        Libros other = (Libros) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.xtecuan.ejemplos.spring.crud.modelo.entidades.Libros[ id=" + id + " ]";
+    }
+    
+}
